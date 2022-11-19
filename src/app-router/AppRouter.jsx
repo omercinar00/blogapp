8bde4f8fa27826a15dd3d444d07793a4453a37f6
ÖmerÇınar <omercinarr00@gmail.com>
2022-11-19 09:32:38 +0300

    router

--- src/app-router/AppRouter.jsx
+++ src/app-router/AppRouter.jsx
@@ -1,22 +1,34 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Navbar from '../components/Navbar';
+import About from '../pages/About';
 import Dashboard from '../pages/Dashboard';
+import Details from '../pages/Details';
 import Login from '../pages/Login'
 import NewBlog from '../pages/NewBlog';
 import Profile from '../pages/Profile';
 import Register from '../pages/Register';
+import UpdateBlog from '../pages/UpdateBlog';
+import PrivateRouter from './PrivateRouter';
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/" element={<Dashboard/>} />
-        <Route path='/addblog' element={<NewBlog/>}/>
-        <Route path='/profile' element={<Profile/>}/>
+
+        <Route path="" element={<PrivateRouter />}>
+          <Route path="/details" element={<Details />} />
+          <Route path="/addblog" element={<NewBlog />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/updateblog" element={<UpdateBlog />} />
+        </Route>
+
+        <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
   );
--- src/app-router/PrivateRouter.jsx
+++ src/app-router/PrivateRouter.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import { Navigate, Outlet } from "react-router";
-import { AuthContext } from "../contexts/AuthContext";
+import { AuthContext } from "../context/AuthContext";
 
 const PrivateRouter = () => {
   const { currentUser } = useContext(AuthContext);
